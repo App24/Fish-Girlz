1aabd3c6ebac7dd7236f56fdd6fac7daf719a4c1
App24 <kado.fofinho2@gmail.com>
2020-11-16 22:18:07 +0000

    Organisation

--- Program.cs
+++ Program.cs
@@ -56,6 +56,7 @@ namespace Fish_Girlz
                 {
                     LogicSystem.Update();
                     CollisionSystem.CheckCollisions();
+                    
                     StateMachine.ActiveState.Update();
                     StateMachine.ActiveState.HandleInput();
                 }
